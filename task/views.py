2581bbf161ce57cf1089c688d3019b264c244f06
mustafa <mustafanaiby@gmail.com>
2023-08-08 11:53:49 +0430

    added template inharetence

--- task/views.py
+++ task/views.py
@@ -3,9 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 tasks = ['Sahil', 'Mustafa', 'Yousof', 'Naib', 'Ali']
+a = 21
+b = 33
 
 
 def index(request):
     return render(request, 'task/task.html', {
-        'tasks': tasks
+        'tasks': tasks,
+        'total': f'{a + b}'
     })
+
+
+def add(request):
+    return render(request, 'task/add.html')
